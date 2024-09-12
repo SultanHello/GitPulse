0549d99e2d433ec3911dc53a23fe5c8ed3683afc
SultanHello <sultanasimbekov@mail.ru>
2024-09-12 19:46:24 +0500

    eureka server

--- EurekaGitService/src/main/java/org/example/eurekagitservice/EurekaGitServiceApplication.java
+++ EurekaGitService/src/main/java/org/example/eurekagitservice/EurekaGitServiceApplication.java
@@ -2,8 +2,10 @@ package org.example.eurekagitservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaGitServiceApplication {
 
     public static void main(String[] args) {
