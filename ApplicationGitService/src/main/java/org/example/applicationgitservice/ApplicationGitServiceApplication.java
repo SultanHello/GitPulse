5606c91e3d6dcb63ce7b511e9eebb5a405a1ec20
SultanHello <sultanasimbekov@mail.ru>
2024-09-13 01:29:45 +0500

    added app

---
+++ ApplicationGitService/src/main/java/org/example/applicationgitservice/ApplicationGitServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.applicationgitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationGitServiceApplication.class, args);
+	}
+
+}
---
+++ ApplicationGitService/src/test/java/org/example/applicationgitservice/ApplicationGitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.applicationgitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationGitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
